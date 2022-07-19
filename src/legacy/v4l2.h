0773cb1391497f402e8bc92a1f8908dae673b795
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-19 13:59:28 -0700

    Style & efficiency cleanup

--- src/legacy/v4l2.h
+++ src/legacy/v4l2.h
@@ -53,43 +53,39 @@
 /* struct for query ctrl and menu */
 typedef struct query_node query_node;
 struct query_node {
-    char * key;
-    void * value;
-    query_node * next;
+  char *key;
+  void *value;
+  query_node *next;
 };
 
 /* struct for uvc camera object */
 typedef struct {
-    int fd;
-    int init;
-    int width;
-    int height;
-    int count;
-    const char * pixelformat; 
-    void ** buffer;
-    int *buf_len;
-    query_node *ctrl_map; 
-    query_node *menu_map;
+  int fd;
+  int init;
+  int width;
+  int height;
+  int count;
+  char const *pixelformat; 
+  void **buffer;
+  int *buf_len;
+  query_node *ctrl_map; 
+  query_node *menu_map;
 } v4l2_device;
 
 
 
-int v4l2_error(const char *error_msg);
-int v4l2_open(const char *device);
-int v4l2_init(v4l2_device * vdev);
-int v4l2_close(v4l2_device * vdev);
-int v4l2_stream_on(v4l2_device * vdev);
-int v4l2_stream_off(v4l2_device * vdev);
-int v4l2_get_ctrl(v4l2_device * vdev, const char *name, int *value);
-int v4l2_set_ctrl(v4l2_device * vdev, const char *name, int value);
-int v4l2_read_frame(v4l2_device * vdev);
-int v4l2_init_mmap(v4l2_device * vdev);
-int v4l2_uninit_mmap(v4l2_device * vdev);
-int v4l2_close_query(v4l2_device * vdev);
-
-//int v4l2_set_ctrl_by_id(int id, int value);
-//int v4l2_read_frame(void);
-//uint8_t* yuyv_rotate(uint8_t* frame);
+int v4l2_error(char const *error_msg);
+int v4l2_open(char const *device);
+int v4l2_init(v4l2_device *vdev);
+int v4l2_close(v4l2_device *vdev);
+int v4l2_stream_on(v4l2_device *vdev);
+int v4l2_stream_off(v4l2_device *vdev);
+int v4l2_get_ctrl(v4l2_device *vdev, char const *name, int *value);
+int v4l2_set_ctrl(v4l2_device *vdev, char const *name, int value);
+int v4l2_read_frame(v4l2_device *vdev);
+int v4l2_init_mmap(v4l2_device *vdev);
+int v4l2_uninit_mmap(v4l2_device *vdev);
+int v4l2_close_query(v4l2_device *vdev);
 
 
 
