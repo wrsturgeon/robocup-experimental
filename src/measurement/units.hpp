1ab90b9b0d364bf22abd8017efecb644e7f650c1
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-19 12:28:23 -0700

    pos_t back to 2d

--- src/measurement/units.hpp
+++ src/measurement/units.hpp
@@ -6,6 +6,8 @@
 
 #include <Eigen/Core> // Eigen::bfloat16
 
+#include <util/options.hpp>
+
 
 
 /**
@@ -30,12 +32,13 @@ class idx_t {
 public:
   idx_t(idx_t const&) = delete;
   idx_t() : internal{0} {}
-  bf16_t meters() const { return static_cast<bf16_t>(ldexpf(internal, lg_cnv)); }
+  idx_t(int16_t mm) : internal{static_cast<int16_t>(mm << lg_cnv)} {}
+  bf16_t meters() const { return static_cast<bf16_t>(ldexpf(internal, -lg_cnv)); }
   operator std::string() const { return std::to_string(meters()) + 'm'; }
 protected:
-  int16_t internal = 0;
+  int16_t internal;
   explicit operator int16_t() const { return internal; }
-  static constexpr int8_t lg_cnv = -11;
+  static constexpr int8_t lg_cnv = 11;
 };
 
 
@@ -43,12 +46,12 @@ protected:
 class pos_t {
 public:
   pos_t(pos_t const&) = delete;
+  pos_t(int16_t x_mm, int16_t y_mm) : x{x_mm}, y{y_mm} {}
   operator std::string() const { return '(' +
         static_cast<std::string>(x) + "x, " +
-        static_cast<std::string>(y) + "y, " +
-        static_cast<std::string>(z) + "z)"; }
+        static_cast<std::string>(y) + "y)"; }
 protected:
-  idx_t x, y, z;
+  idx_t x, y;
 };
 
 
