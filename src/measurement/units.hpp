3f2a0713e8117ef0f170fb264415730a46e93c13
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-19 17:35:42 -0700

    Use half-millimeters as idx_t

--- src/measurement/units.hpp
+++ src/measurement/units.hpp
@@ -1,8 +1,9 @@
 #ifndef MEASUREMENT_UNITS_HPP_
 #define MEASUREMENT_UNITS_HPP_
 
-#include <stdint.h>
+#include <iostream>
 #include <math.h>
+#include <stdint.h>
 
 #include <Eigen/Core> // Eigen::bfloat16
 
@@ -19,39 +20,38 @@ using bf16_t = Eigen::bfloat16;
 
 
 /**
- * Distance from the center of the field.
+ * Half-millimeters from the center of the field.
  * Integral type, not float, so try not to add vanishingly small amounts.
- * On a standard SPL field, 1 metre := 2048 idx_t.
- * Explanation for those interested:
- * - Field dims = {10.4m, 7.4m}; corner-to-corner < 12.8m.
- * - Range of int16_t = [-32768, 32767].
- * - 32767 / 12.8 ~= 2560 < 2048 = (1 << 11).
- *   - Bit shifting is easy and fast
+ * 1 meter := 2000 pos_t
+ * Corner-to-corner field of play ~= 10.8m ~= 21,634 pos_t.
+ * This type is just enough to represent negative-all-the-way-corner-to-corner,
+ * which is exactly what we need to compare any two positions on the field.
  */
-class idx_t {
+class pos_t {
 public:
-  idx_t(idx_t const&) = delete;
-  idx_t() : internal{0} {}
-  idx_t(int16_t mm) : internal{static_cast<int16_t>(mm << lg_cnv)} {}
-  bf16_t meters() const { return static_cast<bf16_t>(ldexpf(internal, -lg_cnv)); }
-  operator std::string() const { return std::to_string(meters()) + 'm'; }
+  // Purposefully no integer conversion ops: must intentionally take pos_t
+  pos_t(pos_t const&) = delete;
+  pos_t(int16_t mm) : internal{mm << 1} { assert((internal >> 1) == mm); }
+  bf16_t mm() const { return bf16_t(ldexpf(internal, -1)); }
+  bf16_t meters() const { return mm() / bf16_t(1000.f); }
+  operator std::string() const { return std::to_string((internal >> 1) / 1000.f) + 'm'; }
+  friend std::ostream& operator<<(std::ostream& os, pos_t const& p) { return os << static_cast<std::string>(p); }
 protected:
   int16_t internal;
   explicit operator int16_t() const { return internal; }
-  static constexpr int8_t lg_cnv = 11;
 };
 
 
 
-class pos_t {
+class Position {
 public:
-  pos_t(pos_t const&) = delete;
-  pos_t(int16_t x_mm, int16_t y_mm) : x{x_mm}, y{y_mm} {}
+  Position(Position const&) = delete;
+  Position(int16_t x_mm, int16_t y_mm) : x{x_mm}, y{y_mm} {}
   operator std::string() const { return '(' +
         static_cast<std::string>(x) + "x, " +
         static_cast<std::string>(y) + "y)"; }
 protected:
-  idx_t x, y;
+  pos_t x, y;
 };
 
 
