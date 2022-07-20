eb2e7ad8cdd008f6109561969252d44c830f1b7b
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-20 13:24:11 +0700

    Add SDL submodule (#15)

--- code_checker.sh
+++ code_checker.sh
@@ -2,9 +2,35 @@
 
 set -e
 
-if grep -rnw ./src -e '#include "'; then
-  echo "Please always use #include <...> starting from ./src (manually included) to preserve links if we move files";
-  exit 1
+EXIT_CODE=0
+
+if grep -Rn ./src -e '#include "'
+then
+  echo "Please always use #include <...> starting from ./src (manually included) to preserve links if we move files"
+  EXIT_CODE=1
 fi
 
-echo "All good!"
+for file in $(find ./src -iname '*.hpp')
+do
+  if [[ "$(head -n1 ${file})" != '#ifndef'* ]]
+  then
+    echo "Please add #include guards to ${file}"
+    EXIT_CODE=1
+  fi
+done
+
+for file in $(find ./src/display -type f)
+do
+  if [ "$(sed '4q;d' ${file})" != '#if DISPLAY' ]
+  then
+    echo -e "Please conditionally compile all ./src/display files with \"#if DISPLAY\"; should be disabled on Nao"
+    EXIT_CODE=1
+  fi
+done
+
+if [ ${EXIT_CODE} -eq 0 ]
+then
+  echo "All good!"
+fi
+
+exit ${EXIT_CODE}
--- src/util/options.hpp
+++ src/util/options.hpp
@@ -5,9 +5,7 @@
 
 // Detects system architecture (32b/64b) and inform various algorithms accordingly.
 // Compile with the compiler argument -D_BITS={32,64} (whichever it is)
-#ifndef _BITS
-#error "Compile with argument -D_BITS=$(getconf LONG_BIT)"
-#else
+#ifdef _BITS
 
 #if _BITS == 32 || _BITS == 64
 #define BITS _BITS
@@ -15,43 +13,66 @@
 #error "Invalid value for _BITS"
 #endif // 32/64
 
-#endif // ...ndef _BITS
-
-
-
-#ifndef TRAINING
-#define TRAINING 0
-#endif // TRAINING
-
-
-
-#ifndef _IMAGE_H
-#error "Compile with argument -D_IMAGE_H=<image height>"
 #else
+#error "Compile with argument -D_BITS=$(getconf LONG_BIT)"
+#endif // ifdef _BITS
+
+
+
+#ifdef _TRAINING
+#define TRAINING _TRAINING
+#else
+#define TRAINING 0
+#endif // ifdef _TRAINING
+
+
+
+#ifdef _IMAGE_H
+
 #if _IMAGE_H > 0
 #define IMAGE_H _IMAGE_H
 #else
 #error "_IMAGE_H <= 0"
 #endif // _IMAGE_H > 0
-#endif // ...ndef _IMAGE_H
 
-#ifndef _IMAGE_W
-#error "Compile with argument -D_IMAGE_W=<image width>"
 #else
+#error "Compile with argument -D_IMAGE_H=<image height>"
+#endif // ifdef _IMAGE_H
+
+
+
+#ifdef _IMAGE_W
+
 #if _IMAGE_W > 0
 #define IMAGE_W _IMAGE_W
 #else
 #error "_IMAGE_W <= 0"
 #endif // _IMAGE_W > 0
-#endif // ...ndef _IMAGE_W
+#else
 
-#define IMAGE_DIAG ((IMAGE_W * IMAGE_W) + (IMAGE_H * IMAGE_H))
+#error "Compile with argument -D_IMAGE_W=<image width>"
+#endif // ifdef _IMAGE_W
 
 
 
-#ifndef NAO_HEIGHT_MM
-#define NAO_HEIGHT_MM 500 // TODO: this is a ROUGH ESTIMATE
-#endif // NAO_HEIGHT_MM
+#define IMAGE_DIAG_SQ ((IMAGE_W * IMAGE_W) + (IMAGE_H * IMAGE_H))
+
+
+
+#ifdef _NAO_HEIGHT_MM
+#define NAO_HEIGHT_MM _NAO_HEIGHT_MM
+#else
+// TODO: this is a ROUGH ESTIMATE
+#define NAO_HEIGHT_MM 500
+#endif // ifdef _NAO_HEIGHT_MM
+
+
+
+#ifdef _DISPLAY
+#define DISPLAY _DISPLAY
+#else
+#define DISPLAY 0
+#endif // ifdef _DISPLAY
 
 
 
