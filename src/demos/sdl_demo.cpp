3b8049e95f6c8b0149cc813c10399e1c59e22e12
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-22 16:34:05 -0700

    Update SDL demo to please almighty CI

--- src/demos/sdl_demo.cpp
+++ src/demos/sdl_demo.cpp
@@ -1,6 +1,8 @@
 #include <sdl/window.hpp>
 #include <vision/image_api.hpp>
 int main() {
-  vision::NaoImage<320, 240> im;
-  sdl::Window bs(im);
+  static constexpr vision::pxidx_t w = 640, h = 480;
+  vision::NaoImage<w, h> im;
+  // Eventually, load some image here (through Eigen)
+  sdl::Window{w, h}.display(im, true);
 }
