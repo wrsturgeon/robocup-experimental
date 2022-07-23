df7c224466bc71a46280e7e719ea308ace6a4a22
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-22 17:50:51 -0700

    Separate Wasserstein folder with README

---
+++ src/wasserstein/wasserstein.hpp
@@ -0,0 +1,13 @@
+#if WASSERSTEIN_ENABLED
+#ifndef WASSERSTEIN_WASSERSTEIN_HPP_
+#define WASSERSTEIN_WASSERSTEIN_HPP_
+
+namespace wasserstein {
+
+} // namespace wasserstein
+
+#endif // WASSERSTEIN_WASSERSTEIN_HPP_
+
+#else // WASSERSTEIN_ENABLED
+#pragma message("Skipping wasserstein.hpp; wasserstein module disabled")
+#endif // WASSERSTEIN_ENABLED
