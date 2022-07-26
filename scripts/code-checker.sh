bd81e81de83d2fbf6227d6bee110880407f0cc86
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-26 13:03:48 -0700

    Add (& mandate) test files for each source file

--- scripts/code-checker.sh
+++ scripts/code-checker.sh
@@ -113,6 +113,16 @@ do
       fi
     fi
 
+    if [ ${dirname} != 'sdl' ]
+    then
+      TEST_FILE="./test/src/${dirname}/${filename::${#filename}-3}cpp"
+      if [ ! -f ${TEST_FILE} ]
+      then
+        echo "Please add ${TEST_FILE} to test ./src/${dirname}/${filename}"
+        EXIT_CODE=1
+      fi
+    fi
+
   done
 done
 
--- test/sdl_demo.cpp
+++
@@ -1,8 +0,0 @@
-#include "sdl/window.hpp"
-#include "vision/image_api.hpp"
-int main() {
-  static constexpr vision::pxidx_t w = 640, h = 480;
-  vision::NaoImage<w, h> im;
-  // Eventually, load some image here (through Eigen)
-  sdl::Window{w, h};//.display(im, true);
-}
