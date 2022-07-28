d5db5ac4548b000610a361a976fa2ea2fb471a0d
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-28 14:09:39 -0700

    Split `rnd/xoshiro`

--- include/rnd/xoshiro.hpp
+++ include/rnd/xoshiro.hpp
@@ -3,104 +3,26 @@
 #define RND_XOSHIRO_HPP_
 
 // Some edits to Blackman & Vigna's xoshiro PRNGs.
+// See the below 64b & 32b sections for links.
 
 #include <stdint.h>
 
 namespace rnd {
 
-
-
-#if BITS == 64
-
+#if BITS == 64 // https://prng.di.unimi.it/xoshiro256plusplus.c
 using t = uint64_t; // rnd::t
-
-// https://prng.di.unimi.it/xoshiro256plusplus.c
-
-/*  Written in 2019 by David Blackman and Sebastiano Vigna (vigna@acm.org)
-
-To the extent possible under law, the author has dedicated all copyright
-and related and neighboring rights to this software to the public domain
-worldwide. This software is distributed without any warranty.
-
-See <http://creativecommons.org/publicdomain/zero/1.0/>. */
-
-template <uint8_t k>
-INLINE constexpr rnd::t rotl(const rnd::t x) { return (x << k) | (x >> (64 - k)); }
-
-static rnd::t s[4] = {0xe220a8397b1dcdaf, 0x6e789e6aa1b965f4, 0x6c45d188009454f, 0xf88bb8a8724c81ec};
-
-INLINE rnd::t next() {
-	const rnd::t result = rotl<23>(s[0] + s[3]) + s[0];
-
-	const rnd::t t = s[1] << 17;
-
-	s[2] ^= s[0];
-	s[3] ^= s[1];
-	s[1] ^= s[2];
-	s[0] ^= s[3];
-
-	s[2] ^= t;
-
-	s[3] = rotl<45>(s[3]);
-
-	return result;
-}
-
-
-
-#elif BITS == 32
-
+#elif BITS == 32 // https://prng.di.unimi.it/xoshiro128plusplus.c
 using t = uint32_t; // rnd::t
-
-// https://prng.di.unimi.it/xoshiro128plusplus.c
-
-/*  Written in 2019 by David Blackman and Sebastiano Vigna (vigna@acm.org)
-
-To the extent possible under law, the author has dedicated all copyright
-and related and neighboring rights to this software to the public domain
-worldwide. This software is distributed without any warranty.
-
-See <http://creativecommons.org/publicdomain/zero/1.0/>. */
-
-template <uint8_t k>
-INLINE constexpr rnd::t rotl(const rnd::t x) { return (x << k) | (x >> (32 - k)); }
-
-static rnd::t s[4] = {0x7b1dcdaf, 0xa1b965f4, 0x8009454f, 0x724c81ec};
-
-INLINE rnd::t next() {
-	const rnd::t result = rotl<7>(s[0] + s[3]) + s[0];
-
-	const rnd::t t = s[1] << 9;
-
-	s[2] ^= s[0];
-	s[3] ^= s[1];
-	s[1] ^= s[2];
-	s[0] ^= s[3];
-
-	s[2] ^= t;
-
-	s[3] = rotl<11>(s[3]);
-
-	return result;
-}
-
-
-
 #endif // 32b/64b
 
+static t s[4];
 
+template <uint8_t k>
+INLINE constexpr t rotl(t const x);
 
-INLINE uint8_t bit() {
-  static rnd::t state;
-  static uint8_t uses;
-  if (!uses) {
-    uses = BITS - 1;
-    state = rnd::next();
-  } else { --uses; }
-  uint8_t rtn = state & 1;
-  state >>= 1;
-  return rtn;
-}
+INLINE t next();
+
+INLINE bool bit();
 
 
 
