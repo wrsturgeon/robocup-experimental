96907775b039c75291c5a145dfc6eba0f8c09bf5
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-28 15:14:24 -0700

    Split `training/scrambler`

---
+++ src/training/scrambler.hpp
@@ -0,0 +1,25 @@
+#include "training/scrambler.hpp"
+
+namespace training {
+
+
+
+template <typename T, uint8_t abits>
+Scrambler<T, abits>::Scrambler() : rnd_uses_left{0} {}
+
+
+
+template <typename T, uint8_t abits>
+MEMBER_INLINE T const* Scrambler<T, abits>::store_and_recall(T const *const current) {
+  if (!rnd_uses_left) {
+    rnd_uses_left = n_renew - 1;
+    rnd_state = rnd::next();
+  } else { --rnd_uses_left; }
+  rnd::t rndidx = rnd_state & bitmask;
+  rnd_state >>= abits;
+  T const *const tmp = data[rndidx];
+  data[rndidx] = current;
+  return tmp;
+}
+
+} // namespace training
