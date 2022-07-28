80ea33c0e442fe7a36bb637b117d2aa2d0509e70
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-28 15:05:12 -0700

    Split `sdl/window`

--- include/sdl/window.hpp
+++ include/sdl/window.hpp
@@ -31,17 +31,11 @@ namespace sdl {
 
 
 
-struct BorderSizes {
-  int left, right, top, bottom;
-};
+struct BorderSizes { int left, right, top, bottom; };
 
-struct Coordinate {
-  int x, y; // SDL uses raw ints
-};
+struct Coordinate { int x, y; };
 
-struct ColorData {
-  uint16_t r, g, b;
-};
+struct ColorData { uint16_t r, g, b; };
 
 
 
@@ -49,7 +43,7 @@ class Window {
 public:
   Window(Window const&) = delete;
   Window(int w, int h, char const *const title = "UPennalizers");
-  ~Window() { SDL_DestroyWindow(window); }
+  ~Window();
   MEMBER_INLINE void display(SDL_Surface* surface, bool popup = false);
 protected:
 
@@ -59,7 +53,7 @@ protected:
 
   // Custom member functions
   MEMBER_INLINE void handle_events();
-  class Closed : public std::runtime_error { using std::runtime_error::runtime_error; };
+  class Closed;
 
   // Ported from C-style SDL
   MEMBER_INLINE BorderSizes border_sizes();
@@ -69,131 +63,16 @@ protected:
   MEMBER_INLINE uint32_t id();
   MEMBER_INLINE float opacity();
   MEMBER_INLINE uint32_t pixel_format();
-  MEMBER_INLINE float brightness() { return SDL_GetWindowBrightness(window); }
-  MEMBER_INLINE void const* data(char const *const name) { return SDL_GetWindowData(window, name); }
-  MEMBER_INLINE uint32_t flags() { return SDL_GetWindowFlags(window); }
-  MEMBER_INLINE bool input_grabbed() { return SDL_GetWindowGrab(window); }
-  MEMBER_INLINE Coordinate max_size() { Coordinate c; SDL_GetWindowMaximumSize(window, &c.x, &c.y); return c; }
-  MEMBER_INLINE Coordinate min_size() { Coordinate c; SDL_GetWindowMinimumSize(window, &c.x, &c.y); return c; }
-  MEMBER_INLINE Coordinate position() { Coordinate c; SDL_GetWindowPosition(window, &c.x, &c.y); return c; }
-  MEMBER_INLINE Coordinate size() { Coordinate c; SDL_GetWindowSize(window, &c.x, &c.y); return c; }
-  MEMBER_INLINE char const* title() { return SDL_GetWindowTitle(window); }
+  MEMBER_INLINE float brightness();
+  MEMBER_INLINE void const* data(char const *const name);
+  MEMBER_INLINE uint32_t flags();
+  MEMBER_INLINE bool input_grabbed();
+  MEMBER_INLINE Coordinate max_size();
+  MEMBER_INLINE Coordinate min_size();
+  MEMBER_INLINE Coordinate position();
+  MEMBER_INLINE Coordinate size();
+  MEMBER_INLINE char const* title();
 };
-
-
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Custom member functions
-
-Window::Window(int w, int h, char const *const title) {
-  window = SDL_CreateWindow(title,
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-#if __APPLE__
-        w >> 1, h >> 1,
-#else
-        w, h,
-#endif
-        SDL_WINDOW_ALLOW_HIGHDPI);
-  if (!window) throw std::runtime_error{
-        std::string{"Couldn't create SDL window: "} +
-        SDL_GetError()};
-
-  surface = SDL_GetWindowSurface(window);
-  if (!surface) throw std::runtime_error{
-        std::string{"Couldn't get SDL window surface: "} +
-        SDL_GetError()};
-  
-  handle_events();
-}
-
-
-
-MEMBER_INLINE void Window::handle_events() {
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_QUIT:
-        throw Closed{"Window manually closed"};
-      default:
-        SDL_UpdateWindowSurface(window);
-    }
-  }
-}
-
-
-
-MEMBER_INLINE void Window::display(SDL_Surface* surface, bool popup) {
-  SDL_BlitSurface(surface, nullptr, surface, nullptr);
-
-  if (popup) {
-    try {
-      do {
-        handle_events();
-      } while (true);
-    } catch (Closed) {}
-  }
-}
-
-
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Ported from C-style SDL
-
-MEMBER_INLINE BorderSizes Window::border_sizes() {
-  BorderSizes s;
-  if (SDL_GetWindowBordersSize(window, &s.top, &s.left, &s.bottom, &s.right)) {
-    throw std::runtime_error{
-          std::string{"Couldn't get window border sizes: "} +
-          SDL_GetError()};
-  }
-  return s;
-}
-
-MEMBER_INLINE int Window::display_index() {
-  int r = SDL_GetWindowDisplayIndex(window);
-  if (r < 0) throw std::runtime_error{
-        std::string{"Couldn't get window display index: "} +
-        SDL_GetError()};
-  return r;
-}
-
-MEMBER_INLINE SDL_DisplayMode Window::display_mode() {
-  SDL_DisplayMode m;
-  if (SDL_GetWindowDisplayMode(window, &m)) throw std::runtime_error{
-        std::string{"Couldn't get window display mode: "} +
-        SDL_GetError()};
-  return m;
-}
-
-MEMBER_INLINE ColorData Window::gamma_ramp() {
-  ColorData c;
-  if (SDL_GetWindowGammaRamp(window, &c.r, &c.g, &c.b)) throw std::runtime_error{
-        std::string{"Couldn't get window gamma ramp: "} +
-        SDL_GetError()};
-  return c;
-}
-
-MEMBER_INLINE uint32_t Window::id() {
-  uint32_t r = SDL_GetWindowID(window);
-  if (!r) throw std::runtime_error{
-        std::string{"Couldn't get window ID: "} +
-        SDL_GetError()};
-  return r;
-}
-
-MEMBER_INLINE float Window::opacity() {
-  float r;
-  if (SDL_GetWindowOpacity(window, &r)) throw std::runtime_error{
-        std::string{"Couldn't get window opacity: "} +
-        SDL_GetError()};
-  return r;
-}
-
-MEMBER_INLINE uint32_t Window::pixel_format() {
-  uint32_t r = SDL_GetWindowPixelFormat(window);
-  if (r == SDL_PIXELFORMAT_UNKNOWN) throw std::runtime_error{
-        std::string{"Couldn't get window pixel format: "} +
-        SDL_GetError()};
-  return r;
-}
 
 
 
