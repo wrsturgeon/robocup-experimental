165eff4c2c2674b0bbc285e00121696c1d5be87b
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-07-28 21:55:03 -0700

    Pull dependencies with `make`

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 # Simply clears & recreates ./build/, then copies ./build.mk there and calls it.
 
+.PHONY: run
+
+run: build
+	cd ./build && make
+
 build: ./build.mk
 	rm -rf ./build
 	mkdir ./build
 	ln ./build.mk ./build/Makefile
-	cd ./build && make
--- build.mk
+++ build.mk
@@ -2,5 +2,20 @@
 # Calling `make` will remake the hard link whenever this file changes;
 # otherwise, it's unsafe to call this directly if build.mk changed.
 
+.PHONY: eigen naoqi_driver
+
 OS := $(shell if [ $(shell uname -s) = Darwin ]; then echo mac; else echo linux; fi) # fuck Windows 💪🤝🚫🪟
 BITS := $(shell if [ ${OS} = linux ]; then nproc --all; else sysctl -n hw.ncpu; fi)
+
+# Dependencies
+dependencies: eigen naoqi_driver
+../third-party:
+	mkdir -p ../third-party
+../third-party/eigen: | ../third-party
+	cd ../third-party && git clone https://gitlab.com/libeigen/eigen.git
+../third-party/naoqi_driver: | ../third-party
+	cd ../third-party && git clone https://github.com/ros-naoqi/naoqi_driver
+eigen: ../third-party/eigen
+	cd ../third-party/eigen && git pull
+naoqi_driver: ../third-party/naoqi_driver
+	cd ../third-party/naoqi_driver && git pull
