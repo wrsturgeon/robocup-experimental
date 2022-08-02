9ad279df132f322d79eb473cefc1d262b805a0f7
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 14:17:46 -0700

    Correcting `pos_t` tests

--- test/units.cpp
+++ test/units.cpp
@@ -3,14 +3,15 @@
 
 class UnitsPosTTest : public measure::pos_t, public testing::Test {
 protected:
-  void SetUp() override { internal = 1 << lc; }
+  void SetUp() override { internal = 42; }
 };
 
 TEST(Units, PosTZero) { EXPECT_FLOAT_EQ(measure::pos_t{0}.mm(), 0.f); }
 TEST(Units, PosTOne) { EXPECT_FLOAT_EQ(measure::pos_t{1}.mm(), 1.f); }
+TEST(Units, PosTAnswer) { EXPECT_FLOAT_EQ(measure::pos_t{42}.mm(), 42.f); }
 TEST(Units, PosTOverflow) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x7fff)}, std::overflow_error); }
 TEST(Units, PosTOverflowNegative) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x8000)}, std::overflow_error); }
 TEST(Units, PosTMeters) { EXPECT_FLOAT_EQ(measure::pos_t{1000}.meters(), 1.f); }
 TEST(Units, PosTMetersImprecise) { EXPECT_FLOAT_EQ(measure::pos_t{1}.meters(), 0.001f); }
 TEST(Units, PosTString) { EXPECT_EQ(measure::pos_t{1000}.operator std::string(), "1.000m"); }
-TEST_F(UnitsPosTTest, Int16) { EXPECT_EQ(operator int16_t(), 1); }
+TEST_F(UnitsPosTTest, Int16) { EXPECT_EQ(operator int16_t(), 42); }
