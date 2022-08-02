7b9a35435acaf55b6dc492a175fc790a8d7460d7
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 11:20:36 -0700

    Remove `lsan.supp`

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,7 +11,7 @@ do
   NOX=$(echo ${CPP} | rev | cut -d/ -f1 | cut -d. -f2- | rev) # No extension or path
   echo "Testing ${CPP}..."
   rm -f ./default.profraw
-  LSAN_OPTIONS=suppressions=${DIR}/lsan.supp ./test_${NOX}
+  ./test_${NOX}
   llvm-profdata merge ./default.profraw -o ./${NOX}.profdata || (echo 'No coverage' && exit 1)
   (llvm-cov report ./test_${NOX} --instr-profile=./${NOX}.profdata ${CPP} | sed '3q;d' | xargs ${DIR}/scripts/parse-coverage.sh) || \
   (llvm-cov show ./test_${NOX} --instr-profile=./${NOX}.profdata ${CPP} && exit 1)
