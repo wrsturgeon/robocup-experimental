26523503af06574927cfa922891b21c424b537a7
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 15:28:51 -0700

    Test `measure::Position`

--- test/units.cpp
+++ test/units.cpp
@@ -13,5 +13,9 @@ TEST(Units, PosTOverflow) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x7
 TEST(Units, PosTOverflowNegative) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x8000)}, std::overflow_error); }
 TEST(Units, PosTMeters) { EXPECT_FLOAT_EQ(measure::pos_t{1000}.meters(), 1.f); }
 TEST(Units, PosTMetersImprecise) { EXPECT_FLOAT_EQ(measure::pos_t{1}.meters(), 0.001f); }
-TEST(Units, PosTString) { EXPECT_EQ(measure::pos_t{1000}.operator std::string(), "1.000000m"); }
+TEST(Units, PosTString) { EXPECT_EQ(static_cast<std::string>(measure::pos_t{1000}), "1.000000m"); }
+TEST(Units, PosTStream) { std::ostream os{nullptr}; EXPECT_NO_THROW(os << measure::pos_t{42}); }
 TEST_F(UnitsPosTTest, Int16) { EXPECT_EQ(operator int16_t(), 42); }
+
+TEST(Units, PositionString) { EXPECT_EQ(static_cast<std::string>(measure::Position{1000, 1000}), "(1.000000m, 1.000000m)"); }
+TEST(Units, PositionStream) { std::ostream os{nullptr}; EXPECT_NO_THROW(os << (measure::Position{42, 42})); }
