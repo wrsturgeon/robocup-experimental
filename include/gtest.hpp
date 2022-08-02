80025581a1dede65d96ef981e19a63f55fd0ed9b
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 12:12:28 -0700

    Ignore `-Wdeprecated` in `gtest.hpp`

--- include/gtest.hpp
+++ include/gtest.hpp
@@ -1,12 +1,13 @@
 // No include guards; taken care of in check.sh
 
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wundef"
-#pragma clang diagnostic ignored "-Wpadded"
-#pragma clang diagnostic ignored "-Wglobal-constructors"
-#pragma clang diagnostic ignored "-Wweak-vtables"
 #pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wdeprecated"
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+#pragma clang diagnostic ignored "-Wpadded"
 #pragma clang diagnostic ignored "-Wreserved-identifier"
+#pragma clang diagnostic ignored "-Wundef"
+#pragma clang diagnostic ignored "-Wweak-vtables"
 #include "gtest/gtest.h"
 #pragma clang diagnostic pop
 
