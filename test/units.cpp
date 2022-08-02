77e37cf0291b44ce2f28e0f5e43ef8d0b00b8ace
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 13:12:44 -0700

    Test `measure::pos_t`

--- test/units.cpp
+++ test/units.cpp
@@ -1,2 +1,7 @@
 #include "measure/units.hpp"
 #include "gtest.hpp"
+
+TEST(Units, pos_t_zero) { EXPECT_FLOAT_EQ(measure::pos_t{0}.mm(), 0.f); }
+TEST(Units, pos_t_one) { EXPECT_FLOAT_EQ(measure::pos_t{1}.mm(), 1.f); }
+TEST(Units, pos_t_overflow) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x7fff)}, std::overflow_error); }
+TEST(Units, pos_t_overflow_negative) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x8000)}, std::overflow_error); }
