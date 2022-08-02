9af68546b23ae396b8d495b56458c0fbd51211d6
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 13:28:25 -0700

    Test more `pos_t`

--- test/units.cpp
+++ test/units.cpp
@@ -5,3 +5,7 @@ TEST(Units, pos_t_zero) { EXPECT_FLOAT_EQ(measure::pos_t{0}.mm(), 0.f); }
 TEST(Units, pos_t_one) { EXPECT_FLOAT_EQ(measure::pos_t{1}.mm(), 1.f); }
 TEST(Units, pos_t_overflow) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x7fff)}, std::overflow_error); }
 TEST(Units, pos_t_overflow_negative) { EXPECT_THROW(measure::pos_t{static_cast<int16_t>(0x8000)}, std::overflow_error); }
+TEST(Units, pos_t_meters) { EXPECT_FLOAT_EQ(measure::pos_t{1000}.meters(), 1.f); }
+TEST(Units, pos_t_meters_imprecise) { EXPECT_FLOAT_EQ(measure::pos_t{1}.meters(), 0.001f); }
+TEST(Units, pos_t_string) { EXPECT_EQ(measure::pos_t{1000}.operator std::string(), "1.000m"); }
+TEST(Units, pos_t_int16) { EXPECT_EQ(static_cast<int16_t>(measure::pos_t{1}), 1); }
