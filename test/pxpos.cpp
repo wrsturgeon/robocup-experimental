75dd63a6d64b6da72c112400f9fccd96a389698d
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 19:13:59 -0700

    Correction: radius *squared*

--- test/pxpos.cpp
+++ test/pxpos.cpp
@@ -2,4 +2,4 @@
 #include "gtest.hpp"
 
 TEST(PxPos, PxPos000) { ASSERT_EQ((vision::pxpos_t{0, 0}.r2()), static_cast<uint32_t>(0)); }
-TEST(PxPos, PxPos345) { ASSERT_EQ((vision::pxpos_t{3, 4}.r2()), static_cast<uint32_t>(5)); }
+TEST(PxPos, PxPos345) { ASSERT_EQ((vision::pxpos_t{3, 4}.r2()), static_cast<uint32_t>(25)); }
