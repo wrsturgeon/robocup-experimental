d247da1830f385545bc1170d334e4a8fa0227778
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-02 18:33:00 -0700

    Fill in actual values ^

--- test/xoshiro.cpp
+++ test/xoshiro.cpp
@@ -2,5 +2,5 @@
 #include "gtest.hpp"
 
 TEST(Xoshiro, Rotl1) { ASSERT_EQ(rnd::rotl<1>(42), static_cast<rnd::t>(84)); }
-TEST(Xoshiro, StartSameEveryTimeNext) { ASSERT_EQ(rnd::next(), static_cast<rnd::t>(42)); }
-TEST(Xoshiro, StartSameEveryTimeBit) { ASSERT_EQ(rnd::bit(), static_cast<rnd::t>(42)); }
+TEST(Xoshiro, StartSameEveryTimeNext) { ASSERT_EQ(rnd::next(), static_cast<rnd::t>(5987356902031041503)); }
+TEST(Xoshiro, StartSameEveryTimeBit) { ASSERT_EQ(rnd::bit(), true); }
