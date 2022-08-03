608166c35c848c6074df3d52227a6a6ef1255357
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-03 10:19:37 -0700

    Test basic `image-api` functionality

--- test/image-api.cpp
+++ test/image-api.cpp
@@ -1,2 +1,5 @@
 #include "vision/image-api.hpp"
 #include "gtest.hpp"
+
+TEST(ImageAPI, NaoImageW) { ASSERT_EQ(vision::NaoImage<42, 13>{}.width(), 42); }
+TEST(ImageAPI, NaoImageH) { ASSERT_EQ(vision::NaoImage<9, 42>{}.height(), 42); }
