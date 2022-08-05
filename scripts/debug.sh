e4879a50c7c8614782a751529857908386c4242b
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-04 18:06:35 -0700

    Add a debug script for individual `cpp` files

---
+++ scripts/debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+if [ ${#} -lt 1 ]
+then
+  echo 'Syntax: '${0}' <arguments to clang++>'
+  exit 1
+fi
+
+if [ ! -f ${1} ]
+then
+  echo 'Source file '${1}' not found.'
+  exit 1
+fi
+
+clang++ -g -o debug_x -DBITS=$(getconf LONG_BIT) -O1 -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Werror ${@}
+lldb ./debug_x -b -o run --one-line-on-crash 'thread backtrace'
+rm debug_x
