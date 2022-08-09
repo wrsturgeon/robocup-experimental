7f518224fe3ad384bf3efc4a375dab4fb68101e8
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-09 15:44:49 -0700

    xoshiro

--- include/rnd/xoshiro.hpp
+++ include/rnd/xoshiro.hpp
@@ -3,15 +3,16 @@
 // Some edits to Blackman & Vigna's xoshiro PRNGs.
 // See the below 64b & 32b sections for links.
 
-#include <stdint.h>
+#include <array>
+#include <cstdint>
 
 namespace rnd {
 
-#if BITS == 64       // https://prng.di.unimi.it/xoshiro256plusplus.c
-using t = uint64_t;  // rnd::t
-#elif BITS == 32     // https://prng.di.unimi.it/xoshiro128plusplus.c
-using t = uint32_t;  // rnd::t
-#endif               // 32b/64b
+#if BITS == 64
+using t = std::uint64_t;  // rnd::t
+#elif BITS == 32
+using t = std::uint32_t;  // rnd::t
+#endif  // 32b/64b
 
 auto next() -> t;
 
