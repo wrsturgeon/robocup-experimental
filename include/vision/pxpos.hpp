7a4fef5636cdcbde8517ed0a2a3aed56c1de975e
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-09 14:14:46 -0700

    And `pxpos`

--- include/vision/pxpos.hpp
+++ include/vision/pxpos.hpp
@@ -2,24 +2,25 @@
 
 #include "measure/units.hpp"
 
-#include <stdint.h>
+#include <cstdint>
 #include <iostream>
 #include <string>
 
 namespace vision {
 
-using pxidx_t = int16_t;
+using pxidx_t = std::int16_t;
 
 // (0, 0) is the center of the image; expand outward from there
 class pxpos_t {
  public:
-  pxpos_t(pxpos_t const&) = delete;
-  explicit pxpos_t(pxidx_t x_ = 0, pxidx_t y_ = 0);
-  operator std::string() const;
+  explicit pxpos_t(pxidx_t xpos, pxidx_t ypos);
+  explicit operator std::string() const;
   friend auto operator<<(std::ostream& os, pxpos_t const& p) -> std::ostream&;
-  pxidx_t const x = 0;
-  pxidx_t const y = 0;
-  auto r2() const -> uint32_t;
+  [[nodiscard]] auto r2() const -> std::uint32_t;
+
+ private:
+  pxidx_t const x_;
+  pxidx_t const y_;
 };
 
 }  // namespace vision
--- src/vision/pxpos.cpp
+++ src/vision/pxpos.cpp
@@ -4,18 +4,18 @@ namespace vision {
 
 using pxidx_t = int16_t;
 
-pxpos_t::pxpos_t(pxidx_t x_, pxidx_t y_) : x{x_}, y{y_} {}
+pxpos_t::pxpos_t(pxidx_t xpos, pxidx_t ypos) : x_{xpos}, y_{ypos} {}
 
 pxpos_t::operator std::string() const {
   return '(' +
-         std::to_string(x) + "x, " +
-         std::to_string(y) + "y)";
+         std::to_string(x_) + "x, " +
+         std::to_string(y_) + "y)";
 }
 
 auto pxpos_t::r2() const -> uint32_t {
   return (
-        static_cast<uint32_t>(x * x) +  // overflow-safe
-        static_cast<uint32_t>(y * y));
+        static_cast<uint32_t>(x_ * x_) +  // overflow-safe
+        static_cast<uint32_t>(y_ * y_));
 }
 
 auto operator<<(std::ostream& os, pxpos_t const& p) -> std::ostream& {
