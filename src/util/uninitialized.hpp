c23a10397c054962f02e629dfb623bda8dda0d81
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-08-27 22:31:46 -0400

    Use `std::byte` rather than `std::uint8_t`

--- src/util/uninitialized.hpp
+++ src/util/uninitialized.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <utility>  // std::move
+#include <cstdint>
 
 template <typename T>
 inline __attribute__((always_inline)) auto
 uninitialized() -> T {
   // NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays)
-  uint8_t bytes[sizeof(T)];
+  std::byte bytes[sizeof(T)];
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
   return std::move(*reinterpret_cast<T*>(bytes));
 }
