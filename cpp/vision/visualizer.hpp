9e711fe999d372d4d76713629643fe4b48597780
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-10-28 11:21:03 -0400

    Pull red channel only: furthest from field color (#102)

--- cpp/vision/visualizer.hpp
+++ cpp/vision/visualizer.hpp
@@ -17,23 +17,23 @@ display_estimate(Layer<H, W> const& im, Projection const& proj) {
   // issues with assigning to an Eigen chip, so we'll do it manually--efficiency couldn't matter less here
   for (imsize_t y = 0; y < H; ++y) {
     for (imsize_t x = 0; x < W; ++x) {
-      rgb(y, x, 0) = im(y, x);
-      rgb(y, x, 1) = im(y, x);
-      rgb(y, x, 2) = im(y, x);
+      rgb(y, x, Eigen::fix<0>) = im(y, x);
+      rgb(y, x, Eigen::fix<1>) = im(y, x);
+      rgb(y, x, Eigen::fix<2>) = im(y, x);
     }
   }
   img::idxintdiff_t uv;
   for (std::size_t i = 0; i < N_samples; ++i) {
     // do { uv = proj(measure::sample_field_lines()); } while (std::abs(uv[1]) >= vcenter or std::abs(uv[0]) >= hcenter);
     uv = proj(measure::sample_field_lines());
-    std::cout << "u, v = " << uv[0] << ", " << uv[1] << std::endl;
+    // std::cout << "u, v = " << uv[0] << ", " << uv[1] << std::endl;
     if (std::abs(uv[1]) < vcenter and std::abs(uv[0]) < hcenter) {
       auto u = hcenter + uv[0];
       auto v = vcenter + uv[1];
       // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-      rgb(v, u, 0) = 255;
-      rgb(v, u, 1) = 0;
-      rgb(v, u, 2) = 0;
+      rgb(v, u, Eigen::fix<0>) = 255;
+      rgb(v, u, Eigen::fix<1>) = 0;
+      rgb(v, u, Eigen::fix<2>) = 0;
     }
   }
   img::save<H, W, 3>(rgb.data(), "_estimate.png");
