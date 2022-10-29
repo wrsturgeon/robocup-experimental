d8d6b15590142b85f8a3bd4c93b8edd8f37cb428
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-10-28 22:31:26 -0400

    Manual approximation ... bad one lol

--- cpp/vision/visualizer.hpp
+++ cpp/vision/visualizer.hpp
@@ -12,8 +12,8 @@ namespace vision {
 
 template <ufull_t N_samples, imsize_t H = kImageH, imsize_t W = kImageW> void
 display_estimate(Layer<H, W> const& im, Projection const& proj) {
-  static constexpr auto vcenter = ((H + 1) >> 1);
-  static constexpr auto hcenter = ((W + 1) >> 1);
+  static constexpr imsize_t vcenter = ((H + 1) >> 1);
+  static constexpr imsize_t hcenter = ((W + 1) >> 1);
   Tensor<H, W, 3> rgb;
   // issues with assigning to an Eigen chip, so we'll do it manually--efficiency couldn't matter less here
   for (imsize_t y = 0; y < H; ++y) {
@@ -23,17 +23,19 @@ display_estimate(Layer<H, W> const& im, Projection const& proj) {
       rgb(y, x, Eigen::fix<2>) = im(y, x);
     }
   }
-  std::array<cint<kSystemBits, signed>, 2> uv = uninitialized<std::array<cint<kSystemBits, signed>, 2>>();
+  auto uvw = uninitialized<std::array<cint<kSystemBits, signed>, 3>>();
+  ufull_t u, v;
   for (ufull_t i = 0; i < N_samples; ++i) {
-    // do { uv = proj(measure::sample_field_lines()); } while (std::abs(uv[1]) >= vcenter or std::abs(uv[0]) >= hcenter);
-    uv = proj(measure::sample_field_lines());
-    if ((std::abs(uv[1]) < vcenter) and (std::abs(uv[0]) < hcenter)) {
-      auto u = hcenter + uv[0];
-      auto v = vcenter + uv[1];
-      // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-      rgb(v, u, Eigen::fix<0>) = 255;
-      rgb(v, u, Eigen::fix<1>) = 0;
-      rgb(v, u, Eigen::fix<2>) = 0;
+    uvw = proj(measure::sample_field_lines());
+    if (uvw[2] > 0) {
+      u = static_cast<ufull_t>(hcenter + uvw[0]);
+      v = static_cast<ufull_t>(vcenter + uvw[1]);
+      if ((v < H) and (u < W)) {
+        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+        rgb(v, u, Eigen::fix<0>) = 255;
+        rgb(v, u, Eigen::fix<1>) = 0;
+        rgb(v, u, Eigen::fix<2>) = 0;
+      }
     }
   }
   img::save<H, W, 3>(rgb.data(), "_estimate.png");
