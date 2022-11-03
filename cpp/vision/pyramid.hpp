7a80569d634829b7d12e1f194dc3c59161928c98
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-11-02 20:12:47 -0400

    Find nearest white-enough pixel

--- cpp/vision/pyramid.hpp
+++ cpp/vision/pyramid.hpp
@@ -49,7 +49,104 @@ struct Pyramid {  // NOLINT(altera-struct-pack-align)
   void save(std::filesystem::path const& fpath) const;
 #endif
   pure auto operator()(imsize_t i, imsize_t j) const noexcept -> decltype(auto) { return array(i, j); }
+  template <u8 threshold>
+  pure auto find_imprecise(imsize_t i, imsize_t j) const NOX->std::pair<imsize_s_t, imsize_s_t>;
+  template <u8 threshold>
+  pure auto find_precise(imsize_t i, imsize_t j) const NOX->std::pair<imsize_s_t, imsize_s_t>;  // still O(lg n)
 };
+
+template <imsize_t H, imsize_t W>
+template <u8 threshold>
+pure auto
+Pyramid<H, W>::find_imprecise(imsize_t i, imsize_t j) const NOX->std::pair<imsize_s_t, imsize_s_t> {
+  if (i >= H) { return (j >= W) ? std::pair<imsize_s_t, imsize_s_t>{-1, -1} : std::pair<imsize_s_t, imsize_s_t>{-1, 0}; }
+  if (j >= W) { return {0, -1}; }
+  if (array(i, j) >= threshold) { return {0, 0}; }
+  bool const lsafe = (j >= 1);
+  bool const rsafe = (j < (W - 1));
+  bool const usafe = (i >= 1);
+  bool const dsafe = (i < (H - 1));
+  // This section is incredibly verbose, redundant, and going to lead to a marginally longer executable file, but each execution will be as fast as possible
+  if (usafe) {
+    if (array(i - 1, j) >= threshold) { return {-1, 0}; }
+    if (lsafe) {
+      if (array(i, j - 1) >= threshold) { return {0, -1}; }
+      if (dsafe) {
+        if (array(i + 1, j) >= threshold) { return {1, 0}; }
+        if (rsafe) {  // All 4 directions are safe
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+          if (array(i - 1, j - 1) >= threshold) { return {-1, -1}; }
+          if (array(i + 1, j - 1) >= threshold) { return {1, -1}; }
+          if (array(i - 1, j + 1) >= threshold) { return {-1, 1}; }
+          if (array(i + 1, j + 1) >= threshold) { return {1, 1}; }
+        } else {  // Only right (j + 1) is off the map
+          if (array(i - 1, j - 1) >= threshold) { return {-1, -1}; }
+          if (array(i + 1, j - 1) >= threshold) { return {1, -1}; }
+        }
+      } else {        // Down (i + 1) is off the map
+        if (rsafe) {  // Only down (i + 1) is off the map
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+          if (array(i - 1, j + 1) >= threshold) { return {-1, 1}; }
+          if (array(i - 1, j - 1) >= threshold) { return {-1, -1}; }
+        } else {  // Down (i + 1) and right (j + 1) are off the map
+          if (array(i - 1, j - 1) >= threshold) { return {-1, 1}; }
+        }
+      }
+    } else {  // Left (j - 1) is off the map
+      if (dsafe) {
+        if (array(i + 1, j) >= threshold) { return {1, 0}; }
+        if (rsafe) {  // Only left (j - 1) is off the map
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+          if (array(i + 1, j + 1) >= threshold) { return {1, 1}; }
+          if (array(i - 1, j + 1) >= threshold) { return {-1, 1}; }
+        }       // Left (j - 1) and right (j + 1) are off the map: do nothing
+      } else {  // Down (i + 1) and left (j - 1) are off the map
+        if (rsafe) {
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+          if (array(i - 1, j + 1) >= threshold) { return {-1, 1}; }
+        }  // Down (i + 1), left (j - 1), and right (j + 1) are off the map: do nothing
+      }
+    }
+  } else {  // Up (i - 1) is off the map
+    if (lsafe) {
+      if (array(i, j - 1) >= threshold) { return {0, -1}; }
+      if (dsafe) {
+        if (array(i + 1, j) >= threshold) { return {1, 0}; }
+        if (rsafe) {  // Only up (i - 1) is off the map
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+          if (array(i + 1, j + 1) >= threshold) { return {1, 1}; }
+          if (array(i + 1, j - 1) >= threshold) { return {1, -1}; }
+        } else {  // Up (i - 1) and right (j + 1) are off the map
+          if (array(i + 1, j - 1) >= threshold) { return {1, -1}; }
+        }
+      } else {  // Down (i + 1) and up (i - 1) are off the map
+        if (rsafe) {
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+        }  // Down (i + 1), up (i - 1), and right (j + 1) are off the map: do nothing
+      }
+    } else {  // Up (i - 1) and left (j - 1) are off the map
+      if (dsafe) {
+        if (array(i + 1, j) >= threshold) { return {1, 0}; }
+        if (rsafe) {  // Only up (i - 1) and left (j - 1) are off the map
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+          if (array(i + 1, j + 1) >= threshold) { return {1, 1}; }
+        }       // Up (i - 1), left (j - 1), and right (j + 1) are off the map: do nothing
+      } else {  // Down (i + 1), up (i - 1), and left (j - 1) are off the map
+        if (rsafe) {
+          if (array(i, j + 1) >= threshold) { return {0, 1}; }
+        }  // Down (i + 1), up (i - 1), left (j - 1), and right (j + 1) are off the map: do nothing
+      }
+    }
+  }
+  if constexpr ((H > 1) and (W > 1)) {
+    auto const [ii, jj] = dn.template find_imprecise<threshold>(i >> 1, j >> 1);
+    return {ii << 1, jj << 1};
+  }
+#ifndef NDEBUG
+  throw std::runtime_error{"Pyramid::find... failed: no above-threshold value found"};
+#endif
+  return {0, 0};
+}
 
 #ifndef NDEBUG
 template <imsize_t H, imsize_t W>
