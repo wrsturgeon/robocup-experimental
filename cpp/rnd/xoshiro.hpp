edbf786cf29ce70257d686e5cfd08b34cf3ad2da
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2022-11-03 16:34:08 -0400

    Use `std::tuple` wherever possible (#117)

--- cpp/rnd/xoshiro.hpp
+++ cpp/rnd/xoshiro.hpp
@@ -5,21 +5,13 @@
 
 #include "util/ternary.hpp"
 
-#include <array>
-#include <cstdint>
+#include <tuple>
 
 namespace rnd {
 
 // 64b: https://prng.di.unimi.it/xoshiro256plusplus.c
 // 32b: https://prng.di.unimi.it/xoshiro128plusplus.c
-using t = cint<kSystemBits, unsigned>;  // rnd::t
-inline constexpr t s1 = if32(0x7b1dcdaf, 0xe220a8397b1dcdaf);
-inline constexpr t s2 = if32(0xa1b965f4, 0x6e789e6aa1b965f4);
-inline constexpr t s3 = if32(0x8009454f, 0x06c45d188009454f);
-inline constexpr t s4 = if32(0x724c81ec, 0xf88bb8a8724c81ec);
-inline constexpr u8 r1 = if32(7, 23);
-inline constexpr u8 l1 = if32(9, 17);
-inline constexpr u8 r2 = if32(11, 45);
+using t = ufull_t;  // rnd::t
 
 template <u8 k>
 pure static auto
@@ -27,31 +19,33 @@ rotl(const t x) -> t {
   return (x << k) | (x >> (kSystemBits - k));
 }
 
-[[nodiscard]] static inline auto
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+impure static auto
 next() -> t {
-  // NOLINTBEGIN(readability-magic-numbers)
+  static auto s = std::tuple<t, t, t, t>{
+        if32(0x7b1dcdaf, 0xe220a8397b1dcdaf),  //
+        if32(0xa1b965f4, 0x6e789e6aa1b965f4),
+        if32(0x8009454f, 0x06c45d188009454f),
+        if32(0x724c81ec, 0xf88bb8a8724c81ec)};
 
-  static auto s = std::array<t, 4>{s1, s2, s3, s4};
+  auto const result = t{rotl<if32(7, 23)>(std::get<0>(s) + std::get<3>(s)) + std::get<0>(s)};
 
-  auto const result = t{rotl<r1>(s[0] + s[3]) + s[0]};
+  auto const tmp = t{std::get<1>(s) << if32(9, 17)};
 
-  auto const tmp = t{s[1] << l1};
+  std::get<2>(s) ^= std::get<0>(s);
+  std::get<3>(s) ^= std::get<1>(s);
+  std::get<1>(s) ^= std::get<2>(s);
+  std::get<0>(s) ^= std::get<3>(s);
 
-  s[2] ^= s[0];
-  s[3] ^= s[1];
-  s[1] ^= s[2];
-  s[0] ^= s[3];
+  std::get<2>(s) ^= tmp;
 
-  s[2] ^= tmp;
-
-  s[3] = rotl<r2>(s[3]);
+  std::get<3>(s) = rotl<if32(11, 45)>(std::get<3>(s));
 
   return result;
-
-  // NOLINTEND(readability-magic-numbers)
 }
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 
-// [[nodiscard]] static auto bit() -> bool {
+// impure static auto bit() -> bool {
 //   static t state;
 //   static u8 uses;
 //   if (uses == 0) {
